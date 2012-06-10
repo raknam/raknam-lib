93732e664b7074a8f3029fa255f9a2cef4b7b422
Sylvain TORMENA <github@stormena.fr>
2012-06-10 18:56:35 +0200

    Changed tab to spaces

--- validators/credit-card.php
+++ validators/credit-card.php
@@ -1,15 +1,15 @@
 <?php
-	
-	require_once('../lib/luhnAlgorythm.php');
-	
-	function checkSiret($cc, $debug = false) {
-		$luhn  = luhn($cc);
-		
-		if ($debug) {
-			echo $cc." : ".$luhn." - ".($luhn % 10 == 0 ? "valid" : "invalid");
-		}
-		
-		return $luhn % 10 == 0;
+
+require_once('../lib/luhnAlgorythm.php');
+
+function checkSiret($cc, $debug = false) {
+	$luhn  = luhn($cc);
+
+	if ($debug) {
+		echo $cc." : ".$luhn." - ".($luhn % 10 == 0 ? "valid" : "invalid");
 	}
-	
-	checkSiret("1234567890123452", true);
+
+	return $luhn % 10 == 0;
+}
+
+checkSiret("1234567890123452", true);
--- validators/rio.php
+++ validators/rio.php
@@ -1,6 +1,6 @@
 <?php
 
-	$operators = array(
+$operators = array(
 		"Afone" => "66",
 		"Auchan" => "65",
 		"Bouygues Telecom / Universal Music Mobile / B&You" => "03",
@@ -43,45 +43,45 @@
 		"Colt " => "94",
 		"Ocito " => "95",
 		"Verizon " => "96",
-	);
-	
-	foreach ($operators as $k => $v) $invert_operators[$v] = $k;
+);
 
-	function calcCCC($oo, $quality, $contract, $phone_number) {
-		$str = "".$oo.$quality.$contract.$phone_number;
-		$order = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+";
-		$a = $b = $c = 0;
-		foreach (str_split($str) as $char){
-			$pos = strpos($order, $char);
-			$a = ($a + $pos) % 37;
-			$b = (2 * $b + $pos) % 37;
-			$c = (4 * $c + $pos) % 37;
-		}
-		$order = str_split($order);
-		return $order[$a].$order[$b].$order[$c];
+foreach ($operators as $k => $v) $invert_operators[$v] = $k;
+
+function calcCCC($oo, $quality, $contract, $phone_number) {
+	$str = "".$oo.$quality.$contract.$phone_number;
+	$order = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+";
+	$a = $b = $c = 0;
+	foreach (str_split($str) as $char){
+		$pos = strpos($order, $char);
+		$a = ($a + $pos) % 37;
+		$b = (2 * $b + $pos) % 37;
+		$c = (4 * $c + $pos) % 37;
 	}
-	
-	function checkRIO($rio, $phone_number, $debug = false) {
-		global $invert_operators;
-		
-		$oo       = substr($rio, 0, 2);
-		$quality  = substr($rio, 2, 1);
-		$contract = substr($rio, 3, 6);
-		$checksum = substr($rio, 9, 3);
-		
-		$res = calcCCC($oo, $quality, $contract, $phone_number);
-		
-		if ($debug) {
-			echo sprintf("RIO: %s %s %s %s [%s] : %s - %s<br /><br />", 
-				$oo, $quality, $contract, $checksum, $phone_number, $res, ($res == $checksum ? "valid" : "invalid"));
+	$order = str_split($order);
+	return $order[$a].$order[$b].$order[$c];
+}
+
+function checkRIO($rio, $phone_number, $debug = false) {
+	global $invert_operators;
+
+	$oo       = substr($rio, 0, 2);
+	$quality  = substr($rio, 2, 1);
+	$contract = substr($rio, 3, 6);
+	$checksum = substr($rio, 9, 3);
+
+	$res = calcCCC($oo, $quality, $contract, $phone_number);
+
+	if ($debug) {
+		echo sprintf("RIO: %s %s %s %s [%s] : %s - %s<br /><br />",
+		$oo, $quality, $contract, $checksum, $phone_number, $res, ($res == $checksum ? "valid" : "invalid"));
 			
-			echo "Opérator: ".$oo." ".$invert_operators[$oo]."<br />";
-			echo "Qualité: ".($quality == "P" ? "Particulier" : "Entreprise")."<br />";
-			echo "Numéro de contrat: ".$contract."<br /><br />";
-		}
-		
-		return $res == $checksum;
+		echo "Opérator: ".$oo." ".$invert_operators[$oo]."<br />";
+		echo "Qualité: ".($quality == "P" ? "Particulier" : "Entreprise")."<br />";
+		echo "Numéro de contrat: ".$contract."<br /><br />";
 	}
-	
-	checkRIO("04P1234566M0", "0612345678", true);
-	checkRIO("03P123456559", "0612345678", true);
+
+	return $res == $checksum;
+}
+
+checkRIO("04P1234566M0", "0612345678", true);
+checkRIO("03P123456559", "0612345678", true);
