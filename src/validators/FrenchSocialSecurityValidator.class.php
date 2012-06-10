504c786073685253aa390b5c5ffde9e7c7e713de
Sylvain TORMENA <github@stormena.fr>
2012-06-10 20:36:12 +0200

    Use exceptions

--- src/validators/FrenchSocialSecurityValidator.class.php
+++ src/validators/FrenchSocialSecurityValidator.class.php
@@ -13,25 +13,32 @@ class FrenchSocialSecurityValidator extends RaknamValidator {
     private $checksum;
 
     private $full;
+    private $lastException;
 
-    public function __contruct() {
+    public function __contruct() { }
 
+    public function getLastException() {
+        return $this->lastException;
     }
-
+    
+    /**
+     * Check given data according tohttp://fr.wikipedia.org/wiki/Numéro_de_sécurité_sociale#Signification_des_chiffres_du_NIR
+     * @throws Exception throw a exception if a rule is broken
+     */
     private function checkdata() {
-        if (!is_numeric($this->full)) 	                  return false;
-        if (!in_array($this->sex, array(1,2)))            return false;
-        if ($this->month > 12 && $this->month < 20)       return false;
-        if ($this->month > 42 || $this->month == 0)       return false;
-        if ($this->dept == 96)                            return false;
+        if (!is_numeric($this->full))                     throw new Exception("Not numeric (full)");
+        if (!in_array($this->sex, array(1,2)))            throw new Exception("Invalid Sex");
+        if ($this->month > 12 && $this->month < 20)       throw new Exception("Invalid Month");
+        if ($this->month > 42 || $this->month == 0)       throw new Exception("Invalid Month");
+        if ($this->dept == 96)                            throw new Exception("Invalid Dept");
         if ($this->dept == 97 || $this->dept == 98) {
             $city = substr($this->$city, -2);
-            if ($city == 0 || $city > 90)                 return false;
+            if ($city == 0 || $city > 90)                 throw new Exception("Invalid City");
         } else {
-            if ($this->city == 0 || $this->city > 990)    return false;
+            if ($this->city == 0 || $this->city > 990)    throw new Exception("Invalid City");
         }
-        if ($this->act == 0)                              return false;
-        if ($this->checksum == 0 || $this->checksum > 97) return false;
+        if ($this->act == 0)                              throw new Exception("Invalid Birth Order Act Number");
+        if ($this->checksum == 0 || $this->checksum > 97) throw new Exception("Invalid Checksum");
     }
 
     public function validate($numSS) {
@@ -44,8 +51,13 @@ class FrenchSocialSecurityValidator extends RaknamValidator {
         $this->act      = substr($numSS, 10, 3);
         $this->checksum = substr($numSS, 13, 2);
 
-        if (!$this->checkdata()) return false;
-        return $this->checksumCalc();
+        try {
+            $this->checkdata();
+        } catch (Exception $e) {
+            $this->lastException = $e;
+            return false;
+        }
+        return $this->checksumCalc() == $this->checksum;
     }
 
     private function checksumCalc() {
